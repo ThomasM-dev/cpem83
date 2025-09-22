e9a24434fb179888e5d7fea0d89352c8836b6029
Thomasdevm <Thomasmadariaga2021@outlook.es>
2025-09-22 17:36:32 -0300

    footer implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import NavBar from "./components/NavBar"
+import Footer from './components/Footer'
 import Hone from './screens/Home'
 
 function App() {
 
   return (
     <>
+    <NavBar/>
     <Hone/>
+    <Footer/>
     </>
   )
 }
--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,11 +1,9 @@
 import HeroBanner from "../../components/HeroBanner";
 import HeroDescription from "../../components/HeroDescription";
-import NavBar from "../../components/NavBar";
 import "./Home.css"
 const Home = () => {
   return (
     <div className="Home">
-      <NavBar/>
       <HeroBanner />
       <HeroDescription />
     </div>
